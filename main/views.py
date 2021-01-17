5b9c73f7bb0a60fc05151fe7dd0f5f94b64c97f9
Eldiiar <eldiiar91@gmail>
2021-01-17 11:21:32 +0600

    init

---
+++ main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse
+
+def homepage(request):
+    return HttpResponse("This is my first page")
+
+def x=int(input("Введите число: "))
+if x % 3 == 0 and x % 5 == 0:
+    print("FizzBuzz", end=' ')
+elif x % 3 == 0:
+    print("Fizz", end=' ')
+elif x % 5 == 0:
+    print("Buzz", end=' ')
+else:
+    print(x, end=' ')
